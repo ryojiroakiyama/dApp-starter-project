a038686af26d42dc35704f43f327b6f24c33507f
rakiyama <ryojiro25@gmail.com>
2022-05-25 00:06:14 +0900

    wave count function, contractAddress(デプロイ先アドレス)を追記

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
-/* ethers 変数を */
+/* ethers 変数を使えるようにする */
+import { ethers } from "ethers";
 const App = () => {
   // ユーザーのパブリックウォレットを保存するために使用する状態変数を定義します。
   const [currentAccount, setCurrentAccount] = useState("");
+  const contractAddress = "0x9b155db0368d0AdEa451a8a5A11B2943424399fA"
   console.log("currentAccount: ", currentAccount);
   // window.ethereumにアクセスできることを確認します。
   const checkIfWalletIsConnected = async () => {
@@ -45,6 +47,28 @@ const App = () => {
       console.log(error);
     }
   };
+  // waveの回数をカウントする関数を実装
+  const wave = async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+        let count = await wavePortalContract.getTotalWaves();
+        console.log("Retrieved total wave count...", count.toNumber());
+        console.log("Signer:", signer);
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   // WEBページがロードされたときに下記の関数を実行します。
   useEffect(() => {
     checkIfWalletIsConnected();
@@ -68,7 +92,8 @@ const App = () => {
             ✨
           </span>
         </div>
-        <button className="waveButton" onClick={null}>
+        {/* waveボタンにwave関数を連動させる。*/}
+        <button className="waveButton" onClick={wave}>
           Wave at Me
         </button>
         {/* ウォレットコネクトのボタンを実装 */}
